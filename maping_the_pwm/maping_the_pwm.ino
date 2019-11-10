b1a140cb28e90fb8378922fb9e742ceec4e242b5
richu101 <45835951+richu101@users.noreply.github.com>
2019-11-10 08:20:05 +0530

    Create maping_the_pwm.ino
    I write this code just to know how to map the pwm value fron the rc tx

---
+++ maping_the_pwm/maping_the_pwm.ino
@@ -0,0 +1,15 @@
+const int ch1 = 2;
+int ch1pwm,ch2pwm,ch3pwm,mapch1pwm;
+void setup() {
+// put your setup code here, to run once:
+pinMode(ch1,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+ch1pwm = pulseIn(ch1,HIGH);
+mapch1pwm = map(ch1pwm,994,1981,0,254);
+Serial.println(mapch1pwm);
+delay(500);
+}
